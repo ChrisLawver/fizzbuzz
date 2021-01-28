53fa937722cb7fa167d5816e042cc33f7a04245c
Chris Lawver <chris_lawver@yahoo.com>
2021-01-28 15:28:44 -0500

    completed assignment

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,34 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            bool playGame = true;
+
             Console.WriteLine("Fizz Buzz!");
+
+
+            while (playGame == true)
+            {
+                Console.Write("\nEnter a number: ");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (userNumber % 3 == 0 && userNumber % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (userNumber % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (userNumber % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(Convert.ToString(userNumber));
+                }
+            }
+
         }
     }
 }
